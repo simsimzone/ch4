3aaa367078f7cc2b81ff3a7b24adc28e952e7880
simsimzone <simsimzone@yahoo.co.uk>
2013-11-24 05:29:40 +0300

    delay len increment after \0 if

--- Source4_13.c
+++ Source4_13.c
@@ -6,22 +6,25 @@
 void reverse(char s [])
 {
 	static int len = 0; /*just to get the length implicitly.*/
-	int i = len++;
+	int i = len;
 	char c = s[i];
 	if (s[i] == '\0')
 		return;
+	len++;
 	reverse(s);
-	s[len - i - 2] = c;
+	s[len - i - 1] = c;
 	if (i == 0)
 		len = 0;
 }
 
 void main()
 {
-	char s[] = "sami";
+	char s[] = "kkk", ss[] = "sami";
 	reverse(s);
 	printf("%s\n", s);
-	reverse(s);
-	printf("%s\n", s);
+	reverse(ss);
+	printf("%s\n", ss);
+	reverse(ss);
+	printf("%s\n", ss);
 	getchar();
 }
